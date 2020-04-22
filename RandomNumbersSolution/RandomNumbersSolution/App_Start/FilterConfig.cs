ea0280a80a0b5e2283a4c474dfcc916bdb37de8f
CSJedi <tetiana.zhydeikina@gmail.com>
2020-04-22 12:37:33 +0300

    add project

---
+++ RandomNumbersSolution/RandomNumbersSolution/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RandomNumbersSolution
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ RandomNumbersSolution/RandomNumbersSolution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RandomNumbersSolution.MvcApplication" Language="C#" %>
